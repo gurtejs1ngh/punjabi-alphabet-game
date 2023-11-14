51039df34adb82e25348238cc9c5215c5293148a
Gurtej Singh <gurtej.singh@outlook.com>
2023-11-14 15:54:22 +1300

    Update game.js
    Added touch functionality.

--- game.js
+++ game.js
@@ -1,58 +1,74 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const alphabetContainer = document.getElementById('alphabet-container');
-  const targetContainer = document.getElementById('target-container');
-  const resetButton = document.getElementById('reset-button');
-  const wrongAudio = document.getElementById('wrong-audio');
+    const alphabetContainer = document.getElementById('alphabet-container');
+    const targetContainer = document.getElementById('target-container');
+    const resetButton = document.getElementById('reset-button');
+    const wrongAudio = document.getElementById('wrong-audio');
 
-  let currentLetterIndex = 0;
+    let currentLetterIndex = 0;
     const alphabet = ['ੳ', 'ਅ', 'ੲ', 'ਸ', 'ਹ', 'ਕ', 'ਖ', 'ਗ', 'ਘ', 'ਙ', 'ਚ', 'ਛ', 'ਜ', 'ਝ', 'ਞ', 'ਟ', 'ਠ', 'ਡ', 'ਢ', 'ਣ', 'ਤ', 'ਥ', 'ਦ', 'ਧ', 'ਨ', 'ਪ', 'ਫ', 'ਬ', 'ਭ', 'ਮ', 'ਯ', 'ਰ', 'ਲ', 'ਵ', 'ੜ'];
+    const shuffledAlphabet = [...alphabet].sort(() => Math.random() - 0.5);
 
+    let draggedLetter = null; // To keep track of the dragged letter
 
-  const shuffledAlphabet = [...alphabet].sort(() => Math.random() - 0.5);
+    function initializeGame() {
+        alphabetContainer.innerHTML = '';
+        targetContainer.innerHTML = '';
+        currentLetterIndex = 0;
 
-  function initializeGame() {
-      alphabetContainer.innerHTML = '';
-      targetContainer.innerHTML = '';
-      currentLetterIndex = 0;
+        shuffledAlphabet.forEach(letter => createDraggableLetter(letter));
+    }
 
-      shuffledAlphabet.forEach(letter => createDraggableLetter(letter));
-  }
+    function createDraggableLetter(letter) {
+        const letterDiv = document.createElement('div');
+        letterDiv.classList.add('draggable');
+        letterDiv.textContent = letter;
+        letterDiv.draggable = true; // for mouse-based drag and drop
+        alphabetContainer.appendChild(letterDiv);
 
-  function createDraggableLetter(letter) {
-      const letterDiv = document.createElement('div');
-      letterDiv.classList.add('draggable');
-      letterDiv.textContent = letter;
-      letterDiv.draggable = true;
-      alphabetContainer.appendChild(letterDiv);
+        letterDiv.addEventListener('dragstart', handleDragStart);
+        letterDiv.addEventListener('touchstart', handleTouchStart);
+    }
 
-      letterDiv.addEventListener('dragstart', (e) => {
-          e.dataTransfer.setData('text', letter);
-      });
-  }
+    targetContainer.addEventListener('dragover', handleDragOver);
+    targetContainer.addEventListener('drop', handleDrop);
+    targetContainer.addEventListener('touchmove', handleDragOver);
+    targetContainer.addEventListener('touchend', handleDrop);
 
-  targetContainer.addEventListener('dragover', (e) => {
-      e.preventDefault();
-  });
+    function handleDragStart(e) {
+        draggedLetter = e.target.textContent; // Store the dragged letter
+        e.dataTransfer.setData('text', draggedLetter); // for mouse-based drag and drop
+    }
 
-  targetContainer.addEventListener('drop', (e) => {
-      e.preventDefault();
-      const letter = e.dataTransfer.getData('text');
-      if (letter === alphabet[currentLetterIndex]) {
-          const newDiv = document.createElement('div');
-          newDiv.classList.add('draggable');
-          newDiv.textContent = letter;
-          targetContainer.appendChild(newDiv);
-          currentLetterIndex++;
+    function handleTouchStart(e) {
+        draggedLetter = e.target.textContent; // Store the dragged letter
+        // For touch devices, prevent default behavior like scrolling
+        e.preventDefault();
+    }
 
-          if (currentLetterIndex === alphabet.length) {
-              alert('ਪੈਂਤੀ ਪੂਰੀ ਕਰਨ ਲਈ ਬਹੁਤ ਬਹੁਤ ਵਧਾਈ ਹੋਵੇ!');
-          }
-      } else {
-          wrongAudio.play();
-      }
-  });
+    function handleDragOver(e) {
+        e.preventDefault(); // Necessary for drop to work
+    }
 
-  resetButton.addEventListener('click', initializeGame);
+    function handleDrop(e) {
+        e.preventDefault();
+        // Use the stored draggedLetter for the logic
+        if (draggedLetter === alphabet[currentLetterIndex]) {
+            const newDiv = document.createElement('div');
+            newDiv.classList.add('draggable');
+            newDiv.textContent = draggedLetter;
+            targetContainer.appendChild(newDiv);
+            currentLetterIndex++;
 
-  initializeGame();
+            if (currentLetterIndex === alphabet.length) {
+                alert('ਪੈਂਤੀ ਪੂਰੀ ਕਰਨ ਲਈ ਬਹੁਤ ਬਹੁਤ ਵਧਾਈ ਹੋਵੇ!');
+            }
+        } else {
+            wrongAudio.play();
+        }
+    }
+
+    resetButton.addEventListener('click', initializeGame);
+
+    initializeGame();
 });
+
